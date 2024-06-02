411bb3c40aa0601deb724a33cf484acae1c8dc1e
mairarafiq301 <mairarafiq301@gmail.com>
2024-06-02 03:21:33 -0700

    push project to github

--- table.js
+++ table.js
@@ -2,11 +2,11 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 let myTable = true;
 while (myTable) {
-    let answers = await inquirer.prompt([{
-            name: "tableNum",
-            type: "number",
-            message: " Enter youe table number: "
-        }]);
+    let answers = await inquirer.prompt({
+        name: "tableNum",
+        type: "number",
+        message: " Enter youe table number: "
+    });
     let num = answers.tableNum;
     if (num) {
         console.log(chalk.green(`\n Here is a table of ${num} \n`));
@@ -16,6 +16,17 @@ while (myTable) {
     }
     else {
         console.log(chalk.red("Please enter a numerical input"));
+    }
+    ;
+    let againPrint = await inquirer.prompt({
+        name: "anotherTable",
+        type: "confirm",
+        message: "Do you want to print another table? ",
+        default: false
+    });
+    if (againPrint.anotherTable == false) {
+        myTable = false;
+        console.log(chalk.yellow("\n \t Thankyou for using my App.\t\n"));
     }
 }
 ;
--- table.ts
+++ table.ts
@@ -4,11 +4,11 @@ import chalk from "chalk" ;
 
 let myTable = true ;
 while(myTable){
-let answers = await inquirer.prompt([{
+let answers = await inquirer.prompt({
     name: "tableNum",
     type: "number",
     message: " Enter youe table number: "
-}]);
+});
 let num = answers.tableNum
 if(num){
     console.log(chalk.green(`\n Here is a table of ${num} \n`));
@@ -18,9 +18,18 @@ if(num){
  }
  }else{
     console.log(chalk.red("Please enter a numerical input"));
+};
+
+let againPrint = await inquirer.prompt({
+    name: "anotherTable",
+    type: "confirm",
+    message: "Do you want to print another table? ",
+    default: false
+
+});
+if(againPrint.anotherTable==false){
+    myTable= false ;
+    console.log(chalk.yellow("\n \t Thankyou for using my App.\t\n"))
 }
 
 };
-
-
-
